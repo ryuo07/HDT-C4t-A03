542aa0320ba5719048a1898e5a56a355c6da4cd9
ryuo07 <longg30072002@gmail.com>
2020-05-16 18:21:57 +0700

    upload file

--- int.py
+++
@@ -1,9 +0,0 @@
-prime = int(input("enter a number:"))
-count = 0
-for i in range(1,int((prime**0.5)//1)+1,1):
-    if prime % i == 0 :
-        count += 1
-if count == 1:
-    print(prime, "là số nguyên tố")
-else:
-    print(prime, "không phải số nguyên tố")
